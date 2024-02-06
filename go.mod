f406d63e79216f3b5b1e87e2785148a3c8638e66
tama-repository <epic6638@gmail.com>
2024-02-07 05:54:30 +0700

    init commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tama-repository/learn-go-module-user
+
+go 1.21.6
+
+require github.com/tama-repository/learn-go-module v1.5.1
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	learn_go_module "github.com/tama-repository/learn-go-module"
+)
+
+func main() {
+
+	result := learn_go_module.SumValue(10, 20)
+
+	fmt.Println(result)
+}
